f730738d4712b89230a6dd975787db0c44ec3d66
LHN0911 <laihoangnam2005@gmail.com>
2025-05-27 17:30:51 +0700

    adD

--- console.sql
+++ console.sql
@@ -24,7 +24,7 @@ create table Product
 delimiter //
 create procedure find_all_Category()
 begin
-    select * from Category;
+select * from Category;
 end;
 create procedure find_byId(c_id int)
 begin
@@ -32,44 +32,44 @@ select * from Category where category_id = c_id;
 end;
 create procedure save_Category(c_name varchar(50), c_dec text)
 begin
-    insert into Category( category_name, description)
-        values (c_name,c_dec);
+insert into Category( category_name, description)
+values (c_name,c_dec);
 end;
 create procedure update_Category(c_id int, c_name varchar(50), c_dec text, c_status bit)
 update Category set category_name = c_name, description =c_dec,status = c_status where category_id = c_id;
 delimiter //
 create procedure delete_Category(c_id int)
 begin
-    delete from Category where category_id = c_id;
+delete from Category where category_id = c_id;
 end;
 -- ===
 delimiter //
 create procedure find_all_Product()
 begin
-    select * from Product;
+select * from Product;
 end;
 create procedure find_by_Id(c_id int)
 begin
-    select * from Product where product_id = c_id;
+select * from Product where product_id = c_id;
 end;
-create procedure save_product(c_name varchar(100), c_dep text, c_price double, c_img varchar(255))
+create procedure save_product(c_name varchar(100), c_dep text, c_price double, c_img varchar(255), c_cate int)
 begin
-    insert into Product(product_name, description, price, image_url,category_id)
-    values (c_name,c_dep,c_price,c_img,category_id);
+insert into Product(product_name, description, price, image_url,category_id)
+values (c_name,c_dep,c_price,c_img,c_cate);
 
 end;
 create procedure update_Product(c_id int, c_name varchar(100), c_dep text, c_price double, c_img varchar(255),c_status bit,c_create datetime, c_cate int)
 begin
-    update Product set product_name = c_name,
-                       description = c_dep,
-                       price = c_price,
-                       image_url = c_img,
-                       status = c_status,
-                       created_at = c_create,
-                       category_id = c_cate where category_id = c_id;
+update Product set product_name = c_name,
+                   description = c_dep,
+                   price = c_price,
+                   image_url = c_img,
+                   status = c_status,
+                   created_at = c_create,
+                   category_id = c_cate where category_id = c_id;
 end;
 create procedure delete_product(c_id int)
 begin
-    delete from Product where category_id = c_id;
+delete from Product where category_id = c_id;
 end;
 delimiter //
